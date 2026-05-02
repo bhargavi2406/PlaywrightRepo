099338de4f4535f29c1c839905c3d2c688714828
bhargavi2406 <70903338+bhargavi2406@users.noreply.github.com>
2026-05-02 18:01:25 +0530

    Add files via upload

---
+++ WEEK2/Day1/02array.js
@@ -0,0 +1,29 @@
+let letters=["a","b","c","d","e","f","g"]
+/*letters.push("f","g","h")
+console.log(letters);
+
+
+letters.pop()
+console.log(letters);
+
+letters.unshift("x","y","z")
+console.log(letters);
+
+letters.shift()
+console.log(letters);
+
+
+let res= letters.slice(0,4)
+console.log(res);
+
+arrayVal.splice(1,3,"x","y","z")
+console.log(arrayVal); // [ 'a', 'x', 'y', 'z', 'e' ]
+
+
+arrayVal.splice(3,4,"x","y","z","w")
+console.log(arrayVal); 
+
+*/
+let Arrayname=[10,7,8,9]
+Arrayname.sort((a,b)=>a-b)
+console.log(Arrayname); 
