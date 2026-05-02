16afcb5179ad19a075c6fc405e437d94d058c860
bhargavi2406 <70903338+bhargavi2406@users.noreply.github.com>
2026-05-02 18:10:21 +0530

    Add files via upload

---
+++ WEEK2/Day1/03functions.js
@@ -0,0 +1,70 @@
+//function
+/*function add()
+{
+    let a=10
+    let b=20
+    let c=a+b
+    console.log(`the addition of a,b is ${c}`);
+
+
+}
+add()
+
+//named function 
+function add(a,b)
+{
+    
+    let c=a+b
+return c
+
+}
+console.log(add(100,200))
+
+
+function add(a,b)
+{
+   
+    let c=a+b
+        console.log(`the addition of a,b is ${c}`);
+
+return c
+}
+
+
+console.log(add(100,200))
+
+
+
+//function expression anonymous
+let y=function (a,b)
+{
+    
+    let c=a+b
+        console.log(`the addition of a,b is ${c}`);
+
+return c
+}
+
+
+console.log(y(100,200))
+
+
+
+// arrow function
+let y=(a,b)=>
+{
+    
+    let c=a+b
+        console.log(`the addition of a,b is ${c}`);
+
+return c
+}
+
+
+console.log(y(100,200))
+*/
+
+
+let arrowfun=(a,b)=> a+b
+
+console.log(arrowfun(20,20));
